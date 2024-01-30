6a4a01aeff3361c02599f25c52cd0acbca22dc13
Pitipat-PC <pitipatw@mit.edu>
2024-01-30 13:52:01 -0500

    add fiber fc detail

---
+++ src/fc_fiber.jl
@@ -0,0 +1,20 @@
+"""
+fc′ and fR1 and fR3 relationship
+
+Available strengths
+
+35/45
+
+50/60
+
+80/95
+
+Available
+
+"""
+
+using CSV, DataFrames
+println(@__DIR__)
+fc_fiber  = CSV.read("fc_fiber.csv", DataFrame);
+
+
