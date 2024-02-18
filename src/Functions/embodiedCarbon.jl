7ff673ef927d058cae06e13bbbf311fb0c3d1fa7
Pitipat-PC <pitipatw@mit.edu>
2024-02-18 13:37:27 -0500

    update embodied carbon equation

--- src/Functions/embodiedCarbon.jl
+++ src/Functions/embodiedCarbon.jl
@@ -5,7 +5,12 @@
 Return embodied carbon coefficient of concrete based on fc′ [kgCO2e/m3]
 """
 function fc2e(fc′::Real)::Float64
-    cfc = -0.0626944435544512 * fc′^2 + 10.0086510099949 * fc′ + 84.14807
+    #only valid up to 60MPa.
+    # cfc = -0.0626944435544512 * fc′^2 + 10.0086510099949 * fc′ + 84.14807
+
+    #data on 25-80 MPa -> linearly interpolated to 100MPa.
+    cfc = 1.6947*fc′ + 267.53
+
    return cfc
 end
 
