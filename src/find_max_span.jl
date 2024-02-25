a44c578f9d1576ee51acab49d6a28cecbb552d07
pitipatw <pitipatw@mit.edu>
2024-02-25 18:03:29 -0500

    finish sampling + plot , have to select the right one.

--- src/find_max_span.jl
+++ src/find_max_span.jl
@@ -10,6 +10,7 @@ using CSV, JSON, DataFrames
 using Makie, GLMakie
 include("Functions//Geometry//pixelgeo.jl")
 include("Functions//capacities.jl")
+include("Functions/generalfunctions.jl")
 sections = JSON.parsefile(joinpath(@__DIR__,"pixel_sample.json"), dicttype = Dict{String,Float64});
 
 all_sections =Matrix{Float64}(undef, length(sections), 3)
@@ -42,64 +43,93 @@ set_fR3 = [3.50, 4.45, 5.73]
 set_as = [99.0, 140.0]
 set_fpe = 0:1860/10:1860
 
+set_spans = 1000:500:12000
+
+# n = prod(length.([set_fc′, set_as, set_fpe, set_spans]),ns)
+output = Dict{Int64,Vector{Dict{String, Union{Real,String}}}}()
 global count = 0 
-for x in range(1,10)
-    for y in range(1,10)
-        global count += 1 
-        span = 0
-        L,t,Lc = all_sections[count,:]
-        section = make_Y_layup_section(L,t,Lc)
+for i_section in 1:100
+    global count += 1 
+    span = 0
+    L,t,Lc = all_sections[count,:]
+    section = make_Y_layup_section(L,t,Lc)
 
-        #viz section (ongoing)
-        # for s in section.solids
-        #     a = Axis(ax4[x,y])
-        #     hidespines!(a)
-        #     poly!(s.points)
-        # end
+    #viz section (ongoing)
+    # for s in section.solids
+    #     a = Axis(ax4[x,y])
+    #     hidespines!(a)
+    #     poly!(s.points)
+    # end
 
-        set_dps = 0:L/10:L
-  
+    set_dps = 0:L/10:L
 
-        for i_fc′ in range(1,3):
-            fc′[i] = set_fc′
-            fR1[i] = set_fR1
-            fR3[i] = set_fR3
 
-            for i_dps in eachindex(set_dps)
-                dps = set_dps[i]
-                pt_pos = [-L/2 -dps ; L/2 -dps]
-                for i_as in eachindex(set_as)
-                    pt_area = set_as[i_as]*[1.0,1.0]
-                    for i_fpe in eachindex(set_fpe)
-                        fpe = set_fpe[i_fpe] 
-                        pt_force = fpe*pt_area
+    for i_fc′ in range(1,3)
+        fc′ = set_fc′[i_fc′]
+        fR1 = set_fR1[i_fc′]
+        fR3 = set_fR3[i_fc′]
 
-                        pixelframesection = PixelFrameSection(fc′, fR1, fR3, pt_area, pt_force, pt_pos)
+        for i_dps in eachindex(set_dps)
+            dps = set_dps[i_dps]
+            pt_pos = [-L/2 -dps ; L/2 -dps]
+            for i_as in eachindex(set_as)
+                pt_area = set_as[i_as]*[1.0,1.0]
+                for i_fpe in eachindex(set_fpe)
+                    fpe = set_fpe[i_fpe] 
+                    pt_force = fpe*pt_area
+
+                    pixelframesection = PixelFrameSection(fc′, fR1, fR3, pt_area, pt_force, pt_pos)
+                    
+                    #mu = get_Mu(pixelframesection)
+                    #vu = get_Vu(pixelframesection)
+                    as = sum(pt_area)
+                    mu = get_Mu(section, fc′, as, fpe, dps, L)
+                    vu = get_Vu(section, fc′, fR1, fR3, as, fpe, L,)
+
+                    #Get max span length from point load.
+                    for i_span in eachindex(set_spans) 
+                        span = set_spans[i_span]
+                        #Moment = PL/4
+                        max_f_mu = 4*mu/span
+
+                        #Shear = P/2
+                        max_f_vu = 2*vu
+
+                        check = max_f_mu > max_f_vu 
+                        control = check ? "Shear control" : "Moment control"
+                        load = check ? max_f_vu : max_f_mu
                         
-                        #mu = get_Mu(pixelframesection)
-                        #vu = get_Vu(pixelframesection)
-                        as = sum(pt_area)
-                        mu = get_Mu(section, fc′, as, fpe, dps, L)
-                        vu = get_Vu(section, fc′, fR1, fR3, as, fpe, L,)
+                        this_result = Dict("fc′"=> fc′, "fR1"=> fR1, "fR3"=> fR3, 
+                                        "fpe" => fpe, "as" => as, "dps" => dps,
+                                        "load" => load, "span" => span,
+                                        "L" => L , "t" => "t" , "Lc" => Lc,
+                                        )
+                        if haskey(output, count)
+                            push!(output[count],this_result)
+                        else 
+                            output[count] = [this_result]
+                        end
 
-                        #Get max span length from point load.
-
-                        
                     end
                 end
             end
         end
     end
-
-
-
-            #create a PixelFrame section
-
+end
             
-            
+#visualziaing the result. 
 
+f_output = Figure(size = (500,500))
+ax1 = Axis(f_output[1,1], title = "Span vs Sections")
+
+for i in 1:100
+    # L,t,Lc = all_sections[i,:]
+    for configs in output[i]
+        scatter!(ax1, i, configs["laod"])
     end
 end
+
+
 
 
 
