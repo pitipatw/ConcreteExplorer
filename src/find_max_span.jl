0b77640a7bbe60d7760ae737a16e1832d7c908aa
pitipatw <pitipatw@mit.edu>
2024-02-25 17:24:54 -0500

    add capacityes

--- src/find_max_span.jl
+++ src/find_max_span.jl
@@ -39,8 +39,8 @@ set_fc′ = [40.0, 60.0, 80.0]
 set_fR1 = [2.97, 3.84, 5.11]
 set_fR3 = [3.50, 4.45, 5.73]
 
-
-
+set_as = [99.0, 140.0]
+set_fpe = 0:1860/10:1860
 
 global count = 0 
 for x in range(1,10)
@@ -49,19 +49,52 @@ for x in range(1,10)
         span = 0
         L,t,Lc = all_sections[count,:]
         section = make_Y_layup_section(L,t,Lc)
-        dps = L 
+
+        #viz section (ongoing)
         # for s in section.solids
         #     a = Axis(ax4[x,y])
         #     hidespines!(a)
         #     poly!(s.points)
         # end
-        for i in range(1,3):
+
+        set_dps = 0:L/10:L
+  
+
+        for i_fc′ in range(1,3):
             fc′[i] = set_fc′
             fR1[i] = set_fR1
             fR3[i] = set_fR3
 
+            for i_dps in eachindex(set_dps)
+                dps = set_dps[i]
+                pt_pos = [-L/2 -dps ; L/2 -dps]
+                for i_as in eachindex(set_as)
+                    pt_area = set_as[i_as]*[1.0,1.0]
+                    for i_fpe in eachindex(set_fpe)
+                        fpe = set_fpe[i_fpe] 
+                        pt_force = fpe*pt_area
+
+                        pixelframesection = PixelFrameSection(fc′, fR1, fR3, pt_area, pt_force, pt_pos)
+                        
+                        #mu = get_Mu(pixelframesection)
+                        #vu = get_Vu(pixelframesection)
+                        as = sum(pt_area)
+                        mu = get_Mu(section, fc′, as, fpe, dps, L)
+                        vu = get_Vu(section, fc′, fR1, fR3, as, fpe, L,)
+
+                        #Get max span length from point load.
+
+                        
+                    end
+                end
+            end
+        end
+    end
+
+
+
             #create a PixelFrame section
-            
+
             
             
 
