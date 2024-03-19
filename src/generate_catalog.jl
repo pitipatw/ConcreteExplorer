e9f7bb64e4ce119ec50e7165d37c99f765e34486
pitipatw <pitipatw@mit.edu>
2024-03-18 20:18:00 -0400

    update date

--- src/generate_catalog.jl
+++ src/generate_catalog.jl
@@ -3,7 +3,7 @@ include("Functions//gen_catalog.jl")
 fc′_path = "src//Tables//1503_fiber_for_test.csv"
 results = get_catalog("default", fc′_path = fc′_path)
 println("Saved Here")
-CSV.write(joinpath(@__DIR__, "Catalogs/16_03_catalog_for_test.csv"), results)
+CSV.write(joinpath(@__DIR__, "Catalogs/18_03_catalog_for_test.csv"), results)
 
 #apply some design constraints to the created catalog.
 
