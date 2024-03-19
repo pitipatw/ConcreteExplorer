f703d9817a621b22d92b57efd483ceccf4b91a3a
pitipatw <pitipatw@mit.edu>
2024-03-18 20:17:39 -0400

    update utilization ratio to the plot

--- src/design_pixelframe.jl
+++ src/design_pixelframe.jl
@@ -225,6 +225,15 @@ function plot_element(element_number::Int64, elements_designs::Dict;
     moment_capacity = [elements_designs[element_number][i][:Mu] for i in 1:ns]
     shear_capacity  = [elements_designs[element_number][i][:Vu] for i in 1:ns]
     type = [elements_designs[element_number][i][:T] for i in 1:ns]
+    if type[1] == 2.0
+        type_title = "X2"
+    elseif type[1] == 3.0 
+        type_title = "Y"
+    elseif type[1] == 4.0 
+        type_title = "X4"
+    else
+        print("Invalid type")
+    end
 
     axial_demand  = [demands[i, :pu] for i in sections]
     moment_demand = [demands[i, :mu] for i in sections]
@@ -250,55 +259,93 @@ function plot_element(element_number::Int64, elements_designs::Dict;
 
     f1 = Figure(size=(1200, 600))
     g = f1[1, 1] = GridLayout()
-    axs_design = Axis(g[1, 1], title="Element $element_number", titlesize=20,
-        aspect=DataAspect(),
-        limits=(-1.2*xmax, 1.2*xmax, -2 * L, 1.2*L+25), #used to be 1.2 L
+    axs_design = Axis(g[1, 1], title="Element $type_title $element_number", titlesize=20,
+        #aspect=DataAspect(),
+        limits=(-1.2*xmax, 1.2*xmax+2*res, -2 * L, 1.2*L+25), #used to be 1.2 L
         yticks=div(L, 100)*100:-100:-div(L, 100)*105,
         # yminorticks = IntervalsBetween(2),
         # yminorgridvisible = true,
         ylabel="y"
     )
+
+    axs_axial = Axis(g[2, 1],# aspect=10,
+        limits=(-1.2*xmax, 1.2*xmax+2*res, nothing, nothing), ylabel="Axial [kN]",
+    )
+
+    axs_moment = Axis(g[3, 1], #aspect=10,
+        limits=(-1.2*xmax, 1.2*xmax+2*res, nothing, 500), ylabel="Moment [kNm]",
+    )
+    axs_shear = Axis(g[4, 1], #aspect=10,
+        limits=(-1.2*xmax, 1.2*xmax+2*res, nothing, nothing), ylabel="Shear [kN]",
+    )
+
+    linkxaxes!(axs_design, axs_axial, axs_moment, axs_shear)
+
+    axs_axial_ratio = Axis(g[2, 1], yaxisposition = :right,# aspect=10,
+        limits=(-1.2*xmax, 1.2*xmax+2*res, -0.1, nothing), ylabel="Utilization [ratio]",
+    )
+
+    axs_moment_ratio = Axis(g[3, 1], yaxisposition =:right, #aspect=10,
+        limits=(-1.2*xmax, 1.2*xmax+2*res, -0.1, nothing), ylabel="Utilization [ratio]",
+    )
+    axs_shear_ratio = Axis(g[4, 1], yaxisposition =:right, #aspect=10,
+        limits=(-1.2*xmax, 1.2*xmax+2*res, -0.1, nothing), ylabel="Utilization [ratio]",
+    )
+
+    hidespines!(axs_axial_ratio)
+    hidespines!(axs_moment_ratio)
+    hidespines!(axs_shear_ratio)
+    hideydecorations!(axs_axial_ratio, ticks = false, label =false, ticklabels =false )
+    hideydecorations!(axs_moment_ratio, ticks = false, label =false, ticklabels =false )
+    hideydecorations!(axs_shear_ratio, ticks = false , label =false, ticklabels =false)
 ypos = 0
     points = []
     if type[1] == 3
         for i in 1:n
             points = [x_range[i]-250, -L, 500, L * 1.5]
-            poly!(axs_design, Rect(points...), color=set_fc′[i], colorrange=(20, 100), colormap=:grays, strokecolor= :black,strokewidth = 2)
+            poly!(axs_design, Rect(points...), color=set_fc′[i], 
+            colorrange=(40, 80), colormap=cgrad(:Greys_3, 3, categorical=true), 
+            strokecolor= :black,strokewidth = 2)
 
         end
         section_pts = make_Y_layup_section(L,t,Lc)
         for s in section_pts.solids
-            poly!(axs_design, s.points.+[xmax+250;0], color = :grey)
+            poly!(axs_design, s.points.+[x_n+res+500;0], color = :grey)
         end
         ypos = 150
     elseif type[1] == 2 
         for i in 1:n
             points = [x_range[i]-250, -0.5*L, 500, L]
-            poly!(axs_design, Rect(points...), color=set_fc′[i], colorrange=(20, 100), colormap=:grays, strokecolor= :black,strokewidth = 2)
+            poly!(axs_design, Rect(points...), color=set_fc′[i], 
+            colorrange=(40, 80), colormap=cgrad(:Greys_3, 3, categorical=true), 
+            strokecolor= :black,strokewidth = 2)
 
         end
+        #plot cross section on the side of the beam plot
         section_pts = make_X2_layup_section(L,t,Lc)
         for s in section_pts.solids
-            poly!(axs_design, s.points.+[xmax+250;0], color = :grey)
+            poly!(axs_design, s.points.+[x_n+res+500;0], color = :grey)
             
         end
         ypos = -250
     elseif type[1] == 4
         for i in 1:n
             points = [x_range[i]-250, -L, 500, 2*L]
-            poly!(axs_design, Rect(points...), color=set_fc′[i], colorrange=(20, 100), colormap=:grays, strokecolor= :black,strokewidth = 2)
-
+            poly!(axs_design, Rect(points...), color=set_fc′[i], 
+            colorrange=(40, 80), colormap=cgrad(:Greys_3, 3, categorical=true), 
+            strokecolor= :black,strokewidth = 2)
         end
+        #plot cross section on the side of the beam plot
         section_pts = make_X4_layup_section(L,t,Lc)
         for s in section_pts.solids
-            poly!(axs_design, s.points.+[xmax+250;0], color = :grey)
+            poly!(axs_design, s.points.+[x_n+res+500;0], color = :grey)
         end
         ypos = -350
     else
         println("Invalid Type")
     end
         
-    Colorbar(f1[1, 2], ticks=[40,60,80], colorrange=(40, 80), colormap=cgrad(:grays, 3, categorical=true, rev=true))
+    Colorbar(f1[1, 2], ticks=[40,60,80], colorrange=(40, 100), colormap=cgrad(:Greys_3, 3, categorical=true))
 
 
     tendon = poly!(axs_design, tendon_points, color=:skyblue, alpha=0.5, transparent=true)
@@ -308,30 +355,50 @@ ypos = 0
     #add text description
     txt_force = round(maximum(set_fps.*set_as)/1000,digits = 3) #kN.
     
-    text!(-xmax-500, ypos, text = "Tendon load: "*string(txt_force)*" kN")
-    text!(-xmax+700, ypos, text = "Apply test load "*string(set_P[1])* " kN")
-    text!(-xmax+2000,ypos, text = "Axial post tensioned force "*string(round(set_axial_force[1], digits = 3))* " kN")
-    axs_axial = Axis(g[2, 1], aspect=10,
-        limits=(-1.2*xmax, 1.2*xmax, nothing, nothing), ylabel="Axial [kN]",
-    )
+    text!(axs_design,-xmax-500, ypos, text = "Tendon load: "*string(txt_force)*" kN")
+    text!(axs_design,-xmax+700, ypos, text = "Apply test load "*string(set_P[1])* " kN")
+    text!(axs_design,-xmax+2000,ypos, text = "Axial post tensioned force "*string(round(set_axial_force[1], digits = 3))* " kN")
+    
 
-    axs_moment = Axis(g[3, 1], aspect=10,
-        limits=(-1.2*xmax, 1.2*xmax, nothing, 1000), ylabel="Moment [kNm]",
-    )
-    axs_shear = Axis(g[4, 1], aspect=10,
-        limits=(-1.2*xmax, 1.2*xmax, nothing, nothing), ylabel="Shear [kN]",
-    )
     # hidexdecorations!(axs_design, grid=false)
     # hidexdecorations!(axs_axial, grid=false)
     # hidexdecorations!(axs_moment, grid=false)
 
-    lines!(axs_axial, x_range, axial_capacity, color=:red)
-    lines!(axs_moment, x_range, moment_capacity, color=:blue)
-    lines!(axs_shear, x_range, shear_capacity, color=:green)
+    # lines!(axs_axial, x_range, axial_capacity, color=:red)
+    # lines!(axs_moment, x_range, moment_capacity, color=:blue)
+    # lines!(axs_shear, x_range, shear_capacity, color=:green)
 
-    lines!(axs_axial, x_range, axial_demand, linestyle=:dash, color=:red)
-    lines!(axs_moment, x_range, moment_demand, linestyle=:dash, color=:blue)
-    lines!(axs_shear, x_range, shear_demand, linestyle=:dash, color=:green)
+    # lines!(axs_axial, x_range, axial_demand, linestyle=:dash, color=:red)
+    # lines!(axs_moment, x_range, moment_demand, linestyle=:dash, color=:blue)
+    # lines!(axs_shear, x_range, shear_demand, linestyle=:dash, color=:green)
+
+    stairs!(axs_axial, x_range, axial_capacity, color=:red, step=:center)
+    stairs!(axs_moment, x_range, moment_capacity, color=:blue, step=:center)
+    stairs!(axs_shear, x_range, shear_capacity, color=:green, step=:center)
+
+    stairs!(axs_axial, x_range, axial_demand, linestyle=:dash, color=:red, step=:center)
+    stairs!(axs_moment, x_range, moment_demand, linestyle=:dash, color=:blue, step=:center)
+    stairs!(axs_shear, x_range, shear_demand, linestyle=:dash, color=:green, step=:center)
+
+    # scatter!(axs_axial, x_range, axial_capacity, color=:red, step=:center)
+    # scatter!(axs_moment, x_range, moment_capacity, color=:blue, step=:center)
+    # scatter!(axs_shear, x_range, shear_capacity, color=:green, step=:center)
+
+    # scatter!(axs_axial, x_range, axial_demand, color=:red, step=:center, marker = 'x', markersize = 20)
+    # scatter!(axs_moment, x_range, moment_demand, color=:blue, step=:center, marker = 'x', markersize = 20)
+    # scatter!(axs_shear, x_range, shear_demand, color=:green, step=:center, marker = 'x', markersize = 20)
+
+
+    axial_utilization  = [ axial_demand[i] == 0 ? 0 : axial_capacity[i]/axial_demand[i] for i in eachindex(axial_demand)]
+    moment_utilization = [ moment_demand[i] == 0 ? 0 : moment_capacity[i]/moment_demand[i] for i in eachindex(moment_demand)]
+    shear_utilization  = [ shear_demand[i] == 0 ? 0 : shear_capacity[i]/shear_demand[i] for i in eachindex(shear_demand)]
+
+    # moment_utilization =
+    # shear_utilizatishear
+    scatter!(axs_axial_ratio, x_range, axial_utilization, color=:red, marker = :utriangle)
+    scatter!(axs_moment_ratio, x_range, moment_utilization, color=:blue, marker = :utriangle)
+    scatter!(axs_shear_ratio, x_range, shear_utilization, color=:green, marker = :utriangle)
+
 
 
     # for (i, label) in enumerate(["Axial [kN]", "Moment [kNm]", "Shear [kN]"])
@@ -341,7 +408,7 @@ ypos = 0
 
     rowgap!(g, 10)
 
-    yspace = maximum(tight_yticklabel_spacing!, [axs_axial, axs_shear, axs_moment]) + 10
+    yspace = maximum(tight_yticklabel_spacing!, [axs_axial, axs_shear, axs_moment]) + 15
 
     axs_axial.yticklabelspace = yspace
     axs_moment.yticklabelspace = yspace
@@ -372,10 +439,10 @@ column_design    = elements_designs[max_column[1]]
 
 mymapping = keys(primary_design[1])
 output = DataFrame([name => [] for name in mymapping])
-for i in 1:12
+for i in 1:10
     push!(output,primary_design[i])    
 end
-for i in 1:12
+for i in 1:11
     push!(output,secondary_design[i])    
 end
 for i in 1:12
