9941a33bc21a854248517772464d4b508592885d
pitipatw <pitipatw@mit.edu>
2024-03-18 20:18:08 -0400

    remove unused files

---
+++ src/text_load_calculation.jl
@@ -0,0 +1,19 @@
+#Demand for a secondary beam 
+
+fields = [:pu, :mu, :vu, :ps, :ms, :vs, :dps_max, :type, :s_idx, :v_idx]
+
+#uniformly distributed load
+max_moment_sbeam =  100 #kN
+max_shear_sbeam  =  100 #kN
+max_moment_pbeam =  100 #kN
+max_shear_pbeam  = 100 #kN
+max_axial_c  =  100 #kN
+max_moment_c =  100 #kN
+max_shear_c  =  100 #kN
+
+#width 
+w = 
+
+#Span 
+l = 
+
